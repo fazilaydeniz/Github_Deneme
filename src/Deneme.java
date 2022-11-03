5115fe80c59afdb8e9457f75e1ac4f4e1468c9e6
Fazıl Aydeniz <fazilaydeniz@hotmail.com>
2022-11-03 15:40:22 +0300

    Deneme

---
+++ src/Deneme.java
@@ -0,0 +1,8 @@
+public class Deneme {
+    public static void main(String[] args) {
+
+        System.out.println("Hello World");
+
+        System.out.println("IT Dünyasına Hoşgeldiniz");
+    }
+}
